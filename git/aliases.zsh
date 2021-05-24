a9e5f97888f1c02ad91a57102ce107ada4d617b4
kevin_tucker2 <kevin_tucker2@comcast.com>
2021-05-24 13:45:01 -0400

    update dotfiles

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -1,3 +1,3 @@
-alias -g BRANCHES='$(git branch -l | fzf-tmux --header-lines=1 --reverse --multi --cycle | awk "{print \$1}")'
+alias -g BRANCHES='$(git branch -l | grep -v "^\*"| fzf-tmux | awk "{print \$1}")'
 alias -g deletebranch='git branch -D BRANCHES'
 
