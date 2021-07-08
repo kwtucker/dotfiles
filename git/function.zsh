dbde724b7c244af50c2da3773e69e25e181c4431
kevin_tucker2 <kevin_tucker2@comcast.com>
2021-07-08 09:55:11 -0400

    update dotfiles

--- git/function.zsh
+++ git/function.zsh
@@ -1,6 +1,6 @@
 # movetag 1.0.0
 movetaghead() {
-	git push origin :$1 && git tag -fa -m "moving tag $1" $1 && git push origin master $1
+	git push origin :$1 && git tag -fa -m "moving tag $1" $1 && git push origin main $1
 }
 
 # deletetag 1.0.0
--- whalebyte/function.zsh
+++ whalebyte/function.zsh
@@ -3,9 +3,9 @@
 function notesB() {
 	pw=`pwd`
 	if [ ${#1} -ne 0 ]; then
-		cd ~/.whalebyte/notes && gaa && sleep 2 &&  gcmsg "backup $1" && sleep 2 && git push origin master && sleep 6
+		cd ~/.whalebyte/notes && gaa && sleep 2 &&  gcmsg "backup $1" && sleep 2 && git push origin main && sleep 6
    	else
-		cd ~/.whalebyte/notes && gaa && sleep 2 &&  gcmsg "backup" && sleep 2 && git push origin master && sleep 6
+		cd ~/.whalebyte/notes && gaa && sleep 2 &&  gcmsg "backup" && sleep 2 && git push origin main && sleep 6
    	fi
 	cd $pw
 }
