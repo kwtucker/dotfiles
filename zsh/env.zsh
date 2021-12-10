98ad9c14fe342e8b04f66f99d6057ebfc486228d
kwtucker <kwtucker4@gmail.com>
2021-12-09 20:14:00 -0500

    update dotfiles

--- zsh/env.zsh
+++ zsh/env.zsh
@@ -5,11 +5,6 @@ export HISTFILE="${XDG_DATA_HOME}"/zsh/history
 export HISTSIZE=10000
 export SAVEHIST=${HISTSIZE}
 
-export XDG_CONFIG_HOME=$HOME/.config
-export XDG_CACHE_HOME=$HOME/.cache
-export XDG_DATA_HOME=$HOME/.local/share
-export XDG_RUNTIME_DIR=$HOME/tmp/runtime
-
 export EDITOR=nvim
 export EVENT_NOKQUEUE=1
 export ENV=$USER
@@ -18,6 +13,3 @@ export ENV=$USER
 export CDPATH=$HOME/go/src
 export TEX=/Library/TeX/texbin
 
-export ZSH_THEME=""
-export ZDOTDIR=$XDG_CONFIG_HOME/zsh/
-export ZSH_CACHE_DIR=$XDG_CACHE_HOME/zsh/
