05ae7b861602bd1b1d8d69fe474f617a14dd75e0
kevin.tucker <kevin.tucker@divvypay.com>
2022-07-06 11:12:42 -0400

    update dotfiles

--- kubernetes/function.zsh
+++ kubernetes/function.zsh
@@ -1,12 +1,29 @@
-function loadkubectl() {
-	if [ $commands[kubectl] ]; then 
-		source <(kubectl completion zsh) 
-	fi
+if [ $commands[kubectl] ]; then
+  function kubectl() {
+    source <(command kubectl completion zsh)
+    command kubectl "$@"
+  }
+fi
+
+# Fuzzy match a Kubernetes resource.
+function kfuzz() {
+  kubectl get "$1" | FZF
 }
 
+# Exec into pods container.
+function ke() {
+  local pod=$(echo PODS)
+  local container=$(kubectl get pod $pod -o json | jq '.spec.containers[].name' | tr -d '"' | sort -fd | FZF )
+
+  echo "In $pod:$container"
+  kubectl exec $pod -c $container -it -- bash  
+}
+
+# Manually create job from cronjob.
 function kmcron() {
   local cron=$(echo CRONS)
   local job=${cron}-manual-$(date +%s)
+
   echo "Creating Job from cron job ${cron} with name ${job}"
   kubectl create job $job --from=cronjob/${cron} 
 }
@@ -44,10 +61,10 @@ function kenv() {
 	local ctx=$(kubectl config current-context)
   local cns=$(kubectl config view --minify | grep namespace: | awk 'NR>0 {print $2}')
   local server=$(kubectl config view --minify -o jsonpath='{.clusters[0].cluster.server}')
-  local version=$(kubectl version --short=true)
+  local versions=$(kubectl version --short=true)
   echo "Context:        ${ctx}"
   echo "Namespace:      ${cns}"
-  echo "${version}"
+  echo "${versions}"
   echo "Server URL:     ${server}"
 }
 
