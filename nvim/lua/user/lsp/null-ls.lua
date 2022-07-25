cb77a2ca055bf53377bf1952450787ba9b13ec11
Kevin Tucker <kwtucker4@gmail.com>
2022-07-25 10:29:59 -0400

    update dotfiles

--- nvim/lua/user/lsp/null-ls.lua
+++ nvim/lua/user/lsp/null-ls.lua
@@ -1,6 +1,6 @@
 local null_ls_status_ok, null_ls = pcall(require, "null-ls")
 if not null_ls_status_ok then
-	return
+  return
 end
 
 -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/formatting
@@ -9,17 +9,17 @@ local formatting = null_ls.builtins.formatting
 local diagnostics = null_ls.builtins.diagnostics
 
 null_ls.setup({
-	debug = true,
-	sources = {
-		diagnostics.golangci_lint,
-		diagnostics.staticcheck,
-		diagnostics.flake8,
-		formatting.prettier.with({
-			extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote", "--no-bracket-spacing" },
-		}),
-		formatting.black.with({ extra_args = { "--fast" } }),
-		formatting.stylua,
-		formatting.gofmt,
-		formatting.goimports,
-	},
+  debug = true,
+  sources = {
+    diagnostics.golangci_lint,
+    diagnostics.staticcheck,
+    diagnostics.flake8,
+    formatting.prettier.with({
+      extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote", "--no-bracket-spacing" },
+    }),
+    formatting.black.with({ extra_args = { "--fast" } }),
+    formatting.stylua,
+    formatting.gofmt,
+    formatting.goimports,
+  },
 })
