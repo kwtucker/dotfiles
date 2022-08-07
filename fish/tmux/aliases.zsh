6aa9f05864637b5d0d4eab182618e16dbb03a3f2
kwtucker <kwtucker4@gmail.com>
2022-08-06 20:14:23 -0400

    update

---
+++ fish/tmux/aliases.zsh
@@ -0,0 +1,5 @@
+#! /usr/bin/env zsh
+
+alias tmux="tmux -2 -f ${XDG_CONFIG_HOME}/tmux/tmux.conf"
+alias tk="tmux kill-server"
+alias tls="tmux ls"
---
+++ fish/tmux/install.fish
@@ -0,0 +1,14 @@
+#!/usr/bin/env fish
+
+function cp_to_extra -d "copy to extra"
+    echo $argv | read -l src dest
+    test -e $dest
+        and success "skipped $src"
+        or cp $src $dest
+        and success "copied $src to $dest"
+end
+
+link_file $DOTFILES/fish/tmux/ $HOME/.config/tmux/ backup
+    or abort alacritty link
+
+
