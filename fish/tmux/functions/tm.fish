693c234a6638bb4a799a94242e89e86f14f73f61
kwtucker <kwtucker4@gmail.com>
2022-08-08 21:42:17 -0400

    update

--- fish/tmux/functions/tm.fish
+++ fish/tmux/functions/tm.fish
@@ -9,5 +9,5 @@ function tm -d "create or attach to tmux session." -a name
   if test -n $name
     tmux $change -t "$1" 2>/dev/null || (tmux new-session -d -s $1 && tmux $change -t "$name"); return
   end 
-  session=$(tmux list-sessions -F "#{session_name}" 2>/dev/null | fzf --exit-0) &&  tmux $change -t "$session" || echo "No sessions found."
+  set session $(tmux list-sessions -F "#{session_name}" 2>/dev/null | fzf --exit-0) &&  tmux $change -t "$session" || echo "No sessions found."
 }
